6d3428450cc44f18e99d90cebfd9c67a1e01d684
Kisuk Lee <kisuklee@mit.edu>
2017-02-02 13:28:35 -0500

    add skip ratio to missing section augmentation

--- python/missing_section.py
+++ python/missing_section.py
@@ -21,13 +21,18 @@ class MissingSectionAugment(data_augmentation.DataAugment):
         2. Weight mask modification (put more weight on the missing section(s))
     """
 
-    def __init__(self, max_sec=1):
+    def __init__(self, max_sec=1, skip_ratio=0.3):
         """Initialize MissingSectionAugment."""
         self.set_max_sections(max_sec)
+        self.set_skip_ratio(skip_ratio)
 
     def set_max_translation(self, max_sec):
         """Set the maximum number of missing sections to introduce."""
         self.MAX_SEC = max_sec
+
+    def set_skip_ratio(self, ratio):
+        """Set the probability of skipping augmentation."""
+        self.ratio = ratio
 
     def prepare(self, spec, **kwargs):
         """No change in spec."""
@@ -36,25 +41,28 @@ class MissingSectionAugment(data_augmentation.DataAugment):
 
     def augment(self, sample, **kwargs):
         """Apply missing section data augmentation."""
-        # Randomly draw the number of sections to introduce.
-        num_sec = np.random.randint(0, self.MAX_SEC + 1)
+        ret = sample
 
-        # Assume that the sample contains only one input volume,
-        # or multiple input volumes of same size.
-        imgs  = kwargs['imgs']
-        zdims = set([])
-        for key in imgs:
-            zdim = self.spec[key][-3]
-            assert num_sec < zdim
-            zdims.add(dim)
-        assert len(zdim) == 1
-        zdim = zdims.pop()
+        if np.random.rand() > self.ratio:
+            # Randomly draw the number of sections to introduce.
+            num_sec = np.random.randint(0, self.MAX_SEC + 1)
 
-        # Randomly draw z-slices to black out.
-        locs = np.random.choice(zdim, num_sec, replace=False)
+            # Assume that the sample contains only one input volume,
+            # or multiple input volumes of same size.
+            imgs  = kwargs['imgs']
+            zdims = set([])
+            for key in imgs:
+                zdim = self.spec[key][-3]
+                assert num_sec < zdim
+                zdims.add(dim)
+            assert len(zdim) == 1
+            zdim = zdims.pop()
 
-        # Discard the selected sections.
-        for key in imgs:
-            sample[key][...,locs,:,:] *= 0
+            # Randomly draw z-slices to black out.
+            locs = np.random.choice(zdim, num_sec, replace=False)
 
-        return sample
+            # Discard the selected sections.
+            for key in imgs:
+                ret[key][...,locs,:,:] *= 0
+
+        return ret

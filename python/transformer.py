4881a11b77551715e302724723c64ce24d628d3a
torms3 <kisuklee@mit.edu>
2017-03-02 01:24:19 -0500

    affinity transformer

--- python/transformer.py
+++ python/transformer.py
@@ -16,6 +16,52 @@ class Transformer(object):
         raise NotImplementedError
 
 
+class Affinity(Transformer):
+    """
+    Expand segmentation into affinity represntation.
+    """
+
+    def __init__(self, dst, source, target, crop=None, rebalance=False):
+        """Initialize parameters.
+        Args:
+            dst: List of 3-tuples, each indicating affinity distance in (z,y,x).
+            source: Key to source data from which to construct affinity.
+            target: Key to target data.
+            crop: 3-tuple indicating crop offset.
+            rebalance: Class-rebalanced gradient weight mask.
+        """
+        self.dst = dst
+        self.source = source
+        self.target = target
+        self.crop = crop
+        self.rebalance = rebalance
+
+    def __call__(self, sample):
+        """Affinity label processing."""
+        seg  = sample[self.source]
+        msk  = np.ones_like(seg)
+        affs = list()
+        msks = list()
+        # Affinitize.
+        for dst in self.dst:
+            affs.append(tf.affinitize(seg, dst=dst))
+            msks.append(tf.affinitize_mask(msk, dst=dst))
+        aff = np.concatenate(affs, axis=0)
+        msk = np.concatenate(msks, axis=0)
+        # Rebalancing.
+        if self.rebalance:
+            for c in xrange(aff.shape[0]):
+                msk[c,...] *= tf.rebalance_binary_class(aff[c,...], msk=msk[c,...])
+        # Update sample.
+        sample[self.target] = aff
+        sample[self.target+'_mask'] = msk
+        # Crop.
+        if self.crop is not None:
+            for k, v in sample.iteritems():
+                sample[k] = tf.crop(v, offset=self.crop)
+        return sample
+
+
 class Semantic(Transformer):
     """
     Expand semantic segmentation into multiclass represntation.

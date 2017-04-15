361b819ad4a4bb547e5e0330ba796d7f8ed2a520
torms3 <kisuklee@mit.edu>
2017-04-15 16:59:21 -0400

    update

--- python/test.py
+++ python/test.py
@@ -9,7 +9,7 @@ from vector import Vec3d
 if __name__ == "__main__":
 
     # Data spec path.
-    dspec_path = 'test_spec/pinky.spec'
+    dspec_path = 'test_spec/SNEMI3D.spec'
 
     # Net specification.
     net_spec = dict(input=(18,160,160), label=(18,160,160))
@@ -21,12 +21,12 @@ if __name__ == "__main__":
     # Data augmentation.
     aug_list = list()
     aug_list.append(dict(type='misalign'))
+    aug_list.append(dict(type='box',min_dim=20,max_dim=50,aspect_ratio=6,density=0.3,skip_ratio=0.3,random_color=True))
     aug_list.append(dict(type='missing',max_sec=3,mode='mix',consecutive=True,skip_ratio=0.5))
     aug_list.append(dict(type='missing',max_sec=5,mode='mix',random_color=True,skip_ratio=0.5))
     aug_list.append(dict(type='blur',mode='mix',max_sec=5))
     aug_list.append(dict(type='warp'))
     aug_list.append(dict(type='grey',mode='mix'))
-    aug_list.append(dict(type='box',min_dim=20,max_dim=60,aspect_ratio=6,density=0.3,skip_ratio=0.3,random_color=True))
     aug_list.append(dict(type='flip'))
     params['augment'] = aug_list
 
@@ -39,7 +39,10 @@ if __name__ == "__main__":
         sample = dp.random_sample()
         if True:
             print 'Save as file...'
-            f = h5py.File('sample%.2d.h5' % (i+1))
+            fname = 'sample%.2d.h5' % (i+1)
+            if os.path.exists(fname):
+                os.remove(fname)
+            f = h5py.File(fname)
             for name, data in sample.iteritems():
                 f.create_dataset('/' + name, data=data)
             f.close()
